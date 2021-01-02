74dd2d86c5dbc21f523fd4db5dcf8004f2136b5f
ReZa.Tajari <reza.tajari70@gmail.com>
2021-01-02 11:08:19 +0330

    Created Project

---
+++ FirstShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
